f0a7118e31d5aa449e79648aa9f02b2faf3eb0a9
zhengyingjie <zhengyingjie@le.com>
2019-03-11 09:03:40 +0800

    Initial commit

---
+++ spark-core/word-count/src/main/scala/com/demo/spark/WordCount.scala
@@ -0,0 +1,30 @@
+package com.demo.spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * create by ZhengYingjie on 2019/3/9 11:50
+  */
+object WordCount {
+
+  def main(args: Array[String]): Unit = {
+
+    //新建sparkConf对象
+    val conf = new SparkConf().setAppName("wordCount")
+//      .setMaster("local[*]")
+
+    //创建sparkContext
+    val context = new SparkContext(conf)
+
+    //读取数据 统计
+    val words = context.textFile("RELEASE")
+
+    val res = words.flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_).collect()
+
+    res.foreach(println _)
+
+    //关闭连接
+    context.stop()
+  }
+
+}
